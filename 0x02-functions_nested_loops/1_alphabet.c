caeb829b8be274bf2dbfe9b41cf7e9361f52c8df
olaniyo <opriolani>
2022-10-18 21:46:26 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1_alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alpha in lower case
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
