9c2dc1c490cf1ea1ffc3dda855aeb7a27940a852
olaniyo <opriolani>
2022-10-19 20:29:57 -0700

    abs task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the abs value of int
+ * @n: the int in the question
+ *
+ * Return: the abs of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
