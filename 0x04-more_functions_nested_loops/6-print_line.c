beea875e5b52a6346ffed1e6598cff10ce75f2ac
olaniyo <opriolani>
2022-10-21 14:11:51 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ * @n: the num to be printed
+ */
+void print_line(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
