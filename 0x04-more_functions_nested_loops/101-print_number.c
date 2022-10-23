762454c6e22930b2c555fba98d70857bf474f397
olaniyo <opriolani>
2022-10-23 10:29:14 -0700

    t12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,7 +3,9 @@
 /**
  * print_number - prints an integer
  * @n: input integer parameter
+ *
  */
+
 void print_number(int n)
 {
 	unsigned int i = n;
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,6 +12,6 @@ void print_square(int size);
 void print_numbers(void);
 void print_triangle(int size);
 int _putchar(char c);
-
+void print_number(int n);
 
 #endif
