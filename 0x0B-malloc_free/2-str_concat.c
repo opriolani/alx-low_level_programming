7f989a08a66b92e180f24bbc64323a239d6e335b
olaniyo <opriolani>
2022-11-07 08:24:51 -0800

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *str_concat -> string concatinating function
+ * str_concat -> string concatinating function
  * @s1: string 1
  * @s2: string 2
  * Return: string 1 + string 2
